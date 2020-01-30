aee713b3707896f3aa7c9c768b21657dfc2f3903
maartenbouw100@gmail.com <mbpnlmb08c>
2020-01-30 22:28:47 +0100

    Small changes

--- src/main/java/opdracht/FileReader/XMLReader.java
+++ src/main/java/opdracht/FileReader/XMLReader.java
@@ -25,19 +25,16 @@ public class XMLReader {
             NodeList nList = doc.getElementsByTagName("record");
 
             for (int temp = 0; temp < nList.getLength(); temp++) {
-
                 Node nNode = nList.item(temp);
-
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) nNode;
 
-                    Element eElement = (Element) nNode;
-
-                    int reference = Integer.parseInt(eElement.getAttribute("reference"));
-                    String accountNumber = eElement.getElementsByTagName("accountNumber").item(0).getTextContent();
-                    float startBalance = Float.parseFloat(eElement.getElementsByTagName("startBalance").item(0).getTextContent());
-                    float mutation = Float.parseFloat(eElement.getElementsByTagName("mutation").item(0).getTextContent());
-                    float endBalance = Float.parseFloat(eElement.getElementsByTagName("endBalance").item(0).getTextContent());
-                    String description = eElement.getElementsByTagName("description").item(0).getTextContent();
+                    int reference = Integer.parseInt(element.getAttribute("reference"));
+                    String accountNumber = element.getElementsByTagName("accountNumber").item(0).getTextContent();
+                    float startBalance = Float.parseFloat(element.getElementsByTagName("startBalance").item(0).getTextContent());
+                    float mutation = Float.parseFloat(element.getElementsByTagName("mutation").item(0).getTextContent());
+                    float endBalance = Float.parseFloat(element.getElementsByTagName("endBalance").item(0).getTextContent());
+                    String description = element.getElementsByTagName("description").item(0).getTextContent();
 
                     Transaction transaction = new Transaction(reference, accountNumber, startBalance, mutation, endBalance, description);
                     transactions.add(transaction);
@@ -46,7 +43,6 @@ public class XMLReader {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return transactions;
     }
 }
