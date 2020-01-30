9a280aacf065e5e7be65ad9dd316510e960ffb2f
maartenbouw100@gmail.com <mbpnlmb08c>
2020-01-30 21:52:49 +0100

    CSV Reader added

---
+++ src/main/java/opdracht/CSVReader.java
@@ -0,0 +1,41 @@
+package opdracht;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CSVReader {
+
+    public List<Transaction> getTransactions(File csvFile) {
+        List<List<String>> records = new ArrayList<>();
+        List<Transaction> transactions = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                records.add(Arrays.asList(values));
+            }
+
+            for (int i = 1; i < records.size(); i++) {
+                int reference = Integer.parseInt(records.get(i).get(0));
+                String accountNumber = records.get(i).get(1);
+                String description = records.get(i).get(2);
+                float startBalance = Float.parseFloat(records.get(i).get(3));
+                float mutation = Float.parseFloat(records.get(i).get(4));
+                float endBalance = Float.parseFloat(records.get(i).get(5));
+
+                Transaction transaction = new Transaction(reference, accountNumber, startBalance, mutation, endBalance, description);
+                transactions.add(transaction);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return transactions;
+    }
+}
